671d88f0d845e621e06da0b458013721b3a4d1a8
Kishore Narang <narangkishore98@gmail.com>
2020-07-13 18:38:31 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,233 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Free responsive business website template</title>
+    <link rel="stylesheet" href="static/css/components.css">
+    <link rel="stylesheet" href="static/css/icons.css">
+    <link rel="stylesheet" href="static/css/responsee.css">
+    <link rel="stylesheet" href="static/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="static/owl-carousel/owl.theme.css">
+    <!-- CUSTOM STYLE -->
+    <link rel="stylesheet" href="static/css/template-style.css">
+    <link href="https://fonts.googleapis.com/css?family=Barlow:100,300,400,700,800&amp;subset=latin-ext" rel="stylesheet">
+    <script type="text/javascript" src="static/js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="static/js/jquery-ui.min.js"></script>
+</head>
+<body class="size-1520 primary-color-red background-dark">
+<!-- HEADER -->
+<header class="grid">
+    <!-- Top Navigation -->
+    <nav class="s-12 grid background-none background-primary-hightlight">
+        <!-- logo -->
+        <a href="index.html" class="m-12 l-3 padding-2x logo">
+            <img src="static/images/logo.png">
+        </a>
+        <div class="top-nav s-12 l-9">
+            <ul class="top-ul right chevron">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about-us.php">About Us</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="portfolio.php">Portfolio</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<!-- MAIN -->
+<main role="main">
+    <!-- TOP SECTION -->
+    <section class="grid">
+        <!-- Main Carousel -->
+        <div class="s-12 margin-bottom carousel-fade-transition owl-carousel carousel-main carousel-nav-white carousel-hide-arrows background-dark">
+            <div class="item background-image" style="background-image:url(static/img/carousel-01.jpg)">
+                <p class="text-padding text-strong text-white text-s-size-30 text-size-60 text-uppercase background-primary">We are Web design Heroes</p>
+                <p class="text-padding text-size-20 text-dark text-uppercase background-white">Con nonummy sem integer interdum volutpat dis eget eligendi aliquip dolorum magnam.</p>
+            </div>
+            <div class="item background-image" style="background-image:url(static/img/carousel-02.jpg)">
+                <p class="text-padding text-strong text-white text-s-size-30 text-size-60 text-uppercase background-primary">Inspired by Technologies</p>
+                <p class="text-padding text-size-20 text-dark text-uppercase background-white">Con nonummy sem integer interdum volutpat dis eget eligendi aliquip dolorum magnam.</p>
+            </div>
+            <div class="item background-image" style="background-image:url(static/img/carousel-03.jpg)">
+                <p class="text-padding text-strong text-white text-s-size-30 text-size-60 text-uppercase background-primary">CSS and HTML is our Playground</p>
+                <p class="text-padding text-size-20 text-dark text-uppercase background-white">Con nonummy sem integer interdum volutpat dis eget eligendi aliquip dolorum magnam.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- SECTION 1 -->
+    <section class="grid margin text-center">
+        <a href="/" class="s-12 m-6 l-3 padding-2x vertical-center margin-bottom background-red">
+            <i class="icon-sli-equalizer text-size-60 text-white center margin-bottom-15"></i>
+            <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">User Customization</h3>
+        </a>
+        <a href="/" class="s-12 m-6 l-3 padding-2x vertical-center margin-bottom background-blue">
+            <i class="icon-sli-layers text-size-60 text-white center margin-bottom-15"></i>
+            <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">Affordable Packages</h3>
+        </a>
+
+        <!-- Image-->
+        <img class="m-12 l-6 l-row-2 margin-bottom" src="static/img/img-06.jpg">
+
+        <a href="/" class="s-12 m-6 l-3 padding-2x vertical-center margin-bottom background-orange">
+            <i class="icon-sli-diamond text-size-60 text-white center margin-bottom-15"></i>
+            <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">Premium Features on Demand</h3>
+        </a>
+        <a href="/" class="s-12 m-6 l-3 padding-2x vertical-center margin-bottom background-aqua">
+            <i class="icon-sli-share text-size-60 text-white center margin-bottom-15"></i>
+            <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">Share the Code</h3>
+        </a>
+    </section>
+
+    <!-- SECTION 2 -->
+    <section class="grid section margin-bottom background-dark">
+        <h2 class="s-12 l-6 center text-thin text-size-30 text-white text-uppercase margin-bottom-30">Welcome to the <b>Danish CMS</b></h2>
+        <p class="s-12 l-6 center">Danish CMS is the CMS Project given the class project implemented by Danish Seth. The project is based on Digital Services Providing Company, named Danish CMS. The project is based upon the digital service like digital marketing, website designing, SEO, mobile application development and other website related services. </p>
+    </section>
+
+
+
+    <!-- SECTION OUR TEAM -->
+
+
+    <section class="grid margin">
+        <h2 class="s-12 text-white text-thin text-size-30 text-white text-uppercase margin-top-bottom-20 center text-center">Our <b>Dream Team</b></h2>
+        <div class="s-12 m-6 l-2 margin-bottom">
+            <img src="static/img/team-01.jpg"/>
+            <h4 class="background-primary padding text-strong">Frank Star</h4>
+            <p class="margin-bottom-10 text-primary text-uppercase">Boss</p>
+        </div>
+        <div class="s-12 m-6 l-2 margin-bottom">
+            <img src="static/img/team-02.jpg"/>
+            <h4 class="background-primary padding text-strong">Jane Naismith</h4>
+            <p class="margin-bottom-10 text-primary text-uppercase">Graphic Designer</p>
+        </div>
+        <div class="s-12 m-6 l-2 margin-bottom">
+            <img src="static/img/team-03.jpg"/>
+            <h4 class="background-primary padding text-strong">Mark Stoner</h4>
+            <p class="margin-bottom-10 text-primary text-uppercase">Web Designer</p>
+        </div>
+        <div class="s-12 m-6 l-2 margin-bottom">
+            <img src="static/img/team-04.jpg"/>
+            <h4 class="background-primary padding text-strong">Steve Carpenter</h4>
+            <p class="margin-bottom-10 text-primary text-uppercase">Web Developer</p>
+        </div>
+        <div class="s-12 m-6 l-2 margin-bottom">
+            <img src="static/img/team-05.jpg"/>
+            <h4 class="background-primary padding text-strong">Steff Kingston</h4>
+            <p class="margin-bottom-10 text-primary text-uppercase">Account Manager</p>
+        </div>
+        <div class="s-12 m-6 l-2 margin-bottom">
+            <img src="static/img/team-06.jpg"/>
+            <h4 class="background-primary padding text-strong">John Sandman</h4>
+            <p class="margin-bottom-10 text-primary text-uppercase">Account Manager</p>
+        </div>
+    </section>
+
+
+
+    <!-- SECTION 3 -->
+    <section class="grid margin">
+        <!-- Image-->
+        <h2 class="s-12 text-white text-thin text-size-30 text-white text-uppercase margin-top-bottom-20 center text-center">Our <b>Services</b></h2>
+
+
+       <div class="s-12 m-4">
+           <img class="margin-bottom" src="static/img/img-01.jpg" width="100%">
+
+           <h3 class="text-center">Service Title</h3>
+           <p class="text-center">Service Description</p>
+       </div>
+        <div class="s-12 m-4">
+            <img class="margin-bottom" src="static/img/img-01.jpg" width="100%">
+
+            <h3 class="text-center">Service Title</h3>
+            <p class="text-center">Service Description</p>
+        </div>
+        <div class="s-12 m-4">
+            <img class="margin-bottom" src="static/img/img-01.jpg" width="100%">
+
+            <h3 class="text-center">Service Title</h3>
+            <p class="text-center">Service Description</p>
+        </div>
+    </section>
+
+    <!-- SECTION 4 -->
+    <section class="grid margin margin-top-50">
+        <h2 class="s-12 text-white text-thin text-size-30 text-white text-uppercase margin-top-bottom-20 center text-center"> <b>Portfolio</b></h2>
+
+        <a class="s-12 m-6 margin-bottom" href="gallery.html">
+            <img class="full-img" src="static/img/portfolio/thumb-01.jpg" alt=""/>
+        </a>
+        <a class="s-12 m-6 margin-bottom" href="gallery.html">
+            <img class="full-img" src="static/img/portfolio/thumb-02.jpg" alt=""/>
+        </a>
+        <a class="s-12 m-6 margin-bottom" href="gallery.html">
+            <img class="full-img" src="static/img/portfolio/thumb-03.jpg" alt=""/>
+        </a>
+        <a class="s-12 m-6 margin-bottom" href="gallery.html">
+            <img class="full-img" src="static/img/portfolio/thumb-04.jpg" alt=""/>
+        </a>
+    </section>
+
+    <!-- SECTION 5 -->
+    <section class="grid margin text-center">
+        <div class="m-12 l-4 padding-2x background-dark margin-bottom text-right">
+            <h3 class="text-strong text-size-25 text-uppercase margin-bottom-10">Let's keep in touch</h3>
+            <p>Deleniti pertinacia eu est, te his soluta quaestio pericula illum vulputate lobortis facilisis.</p>
+        </div>
+        <a href="/" class="s-12 m-6 l-2 padding vertical-center margin-bottom facebook hover-zoom">
+            <i class="icon-sli-social-facebook text-size-60 text-white center"></i>
+        </a>
+        <a href="/" class="s-12 m-6 l-2 padding vertical-center margin-bottom twitter hover-zoom">
+            <i class="icon-sli-social-twitter text-size-60 text-white center"></i>
+        </a>
+        <a href="/" class="s-12 m-6 l-2 padding vertical-center margin-bottom youtube hover-zoom">
+            <i class="icon-sli-social-youtube text-size-60 text-white center"></i>
+        </a>
+        <a href="/" class="s-12 m-6 l-2 padding vertical-center margin-bottom linkedin hover-zoom">
+            <i class="icon-sli-social-linkedin text-size-60 text-white center"></i>
+        </a>
+    </section>
+
+</main>
+
+
+<!-- FOOTER -->
+<footer class="grid">
+    <!-- Footer - top -->
+    <!-- Image-->
+    <div class="s-12 l-3 m-row-3 margin-bottom background-image" style="background-image:url(img/img-04.jpg)"></div>
+
+    <div class="s-12 m-9 l-3 padding-2x margin-bottom background-dark">
+        <h2 class="text-strong text-uppercase">Who We Are?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.</p>
+    </div>
+
+    <div class="s-12 m-9 l-3 padding-2x margin-bottom background-dark">
+        <h2 class="text-strong text-uppercase">Where We Are?</h2>
+        <img class="full-img" src="static/img/map.svg" alt=""/>
+    </div>
+
+    <div class="s-12 m-9 l-3 padding-2x margin-bottom background-dark">
+        <h2 class="text-strong text-uppercase">Contact Us</h2>
+        <p><b class="text-primary margin-right-10">P</b> 438 680 4370</p>
+        <p><b class="text-primary margin-right-10">M</b> <a class="text-primary-hover" href="mailto:danishbeta212@gmail.com">danishbeta212@gmail.com</a></p>
+    </div>
+
+    <!-- Footer - bottom -->
+    <div class="s-12 text-center margin-bottom">
+        <p class="text-size-12">Copyright 2020, Danish CMS</p>
+        <p class="text-size-12">All images have been purchased from Bigstock. Do not use the images in your website.</p>
+        <p><a class="text-size-12 text-primary-hover" href="http://www.myresponsee.com" title="Responsee - lightweight responsive framework">Design and coding by Responsee Team</a></p>
+    </div>
+</footer>
+<script type="text/javascript" src="static/js/responsee.js"></script>
+<script type="text/javascript" src="static/owl-carousel/owl.carousel.js"></script>
+<script type="text/javascript" src="static/js/template-scripts.js"></script>
+
+</body>
+</html>
